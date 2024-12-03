ab2bc42b07615a4cf0c886d9e30a2ed62a7f5e30
Sha990989 <anavaneethasankar@gmail.com>
2024-12-03 16:21:37 +0530

    Add files via upload

---
+++ flipping2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class flipping2 {
+    public static void main(String args[])
+    {
+        int a=18;
+        int b=1;
+        System.out.println(a^(1<<b));
+    }
+    
+}
