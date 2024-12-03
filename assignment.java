d75d7fca77c560e31c3ad5a2c37e171660d3df2a
Sha990989 <anavaneethasankar@gmail.com>
2024-12-03 11:03:36 +0530

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+public class assignment {
+    public static void main(String args[])
+    {
+        int a=45;
+        System.out.println(a+=46);
+        System.out.println(a-=9);
+        System.out.println(a*=2);
+        System.out.println(a/=5);
+        System.out.println(a%=3);
+    }
+}
