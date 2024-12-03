bf1ad3e875f56f6fc2a8e319ea41ce549a6767a8
Sha990989 <anavaneethasankar@gmail.com>
2024-12-03 10:11:42 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class arithmetic
+{
+public static void main(String args[])
+{
+    int a=8,b=9,c,d,e,f,g;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    g=a%b;
+    System.out.println("a+b:"+c);
+    System.out.println("a-b:"+d);
+    System.out.println("a*b:"+e);
+    System.out.println("a/b:"+f);
+    System.out.println("a%b:"+g);
+}
+}
